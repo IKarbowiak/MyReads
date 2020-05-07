c88e0bb7d9f4850a44026d42c413f6705136a57e
IKarbowiak <iga.karbowiak@mirumee.com>
2020-05-07 16:54:12 +0200

    Remove static code from App and ListBooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import {Route} from 'react-router-dom'
 import './App.css'
 import ListBooks from './ListBooks'
@@ -7,10 +7,33 @@ import SearchBooks from './SearchBooks'
 
 
 class BooksApp extends React.Component {
+  state = {
+    shelves: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      const shelves = {};
+      console.log('shelves', shelves);
+      books.forEach(book => {
+        if (book.shelf in shelves){
+          shelves[book.shelf].push(book)
+        } else {
+          shelves[book.shelf] = [book]
+        }
+      });
+      this.setState(() => ({
+        shelves
+      }))
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <Route exact path='/' component={ListBooks} />
+        <Route exact path='/' render={() => (
+          <ListBooks shelves={this.state.shelves} />
+        )} />
         <Route path='/search' component={SearchBooks}/>
       </div>
     )
