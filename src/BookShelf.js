516d20967009fd612fca46cab8f526707542cd2b
IKarbowiak <iga.karbowiak@mirumee.com>
2020-05-09 12:26:27 +0200

    Change BookShelf component into stateless functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,30 +1,27 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Book from './Book'
 
+const fromCamelCase = (str, separator=" ") => {
+  const formatted_str = str
+    .replace(/([a-z\d])([A-Z])/g, '$1' + separator + '$2')
+    .toLowerCase()
+  return formatted_str.charAt(0).toUpperCase() + formatted_str.slice(1)
+}
 
-class BookShelf extends Component {
-  fromCamelCase = (str, separator=" ") => {
-    const formatted_str = str
-      .replace(/([a-z\d])([A-Z])/g, '$1' + separator + '$2')
-      .toLowerCase()
-    return formatted_str.charAt(0).toUpperCase() + formatted_str.slice(1)
-  }
-
-  render() {
-    const {shelf_name, books, bookUpdate} = this.props
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.fromCamelCase(shelf_name)}</h2>
-        <ol className="books-grid">
-          {books.map(book => (
-            <li key={book.id}>
-              <Book book={book} bookUpdate={bookUpdate}/>
-            </li>
-          ))}
-        </ol>
-      </div>
-    )
-  }
+const BookShelf = (props) => {
+  const {shelf_name, books, bookUpdate} = props
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{fromCamelCase(shelf_name)}</h2>
+      <ol className="books-grid">
+        {books.map(book => (
+          <li key={book.id}>
+            <Book book={book} bookUpdate={bookUpdate}/>
+          </li>
+        ))}
+      </ol>
+    </div>
+  )
 }
 
 export default BookShelf
